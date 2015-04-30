39c4d8c034a8798942b9d7579891028c0815e18f
Daniel Cousens <github@dcousens.com>
2015-04-30 14:45:27 +1000

    marker: don't recreate marker everytime

--- marker.js
+++ marker.js
@@ -6,10 +6,19 @@ module.exports = React.createClass({
   componentDidMount: function () { this.updateMarker(this.props) },
   componentWillReceiveProps: function (newProps) { this.updateMarker(newProps) },
 
-  updateMarker: function (props) {
-    this.setState({
-      marker: new google.maps.Marker(props)
-    })
+  updateMarker: function (newProps) {
+    var marker = this.state.marker
+
+    // create new marker
+    if (!marker) {
+      marker = new google.maps.Marker(newProps)
+
+    // update existing marker
+    } else {
+      marker.setOptions(newProps)
+    }
+
+    this.setState({ marker: marker })
   },
 
   render: function () {
