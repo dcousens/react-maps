80426aba1484442fc405bf23e3744900734a7832
Daniel Cousens <github@dcousens.com>
2015-07-28 13:03:48 +1000

    marker: avoid re-renders based on resetting the map option

--- marker.js
+++ marker.js
@@ -1,6 +1,7 @@
 /* global google */
 
 var React = require('react')
+var blacklist = require('blacklist')
 
 module.exports = React.createClass({
   componentWillUnmount: function () {
@@ -10,13 +11,22 @@ module.exports = React.createClass({
   },
 
   render: function () {
+    var props = this.props
+    if (!props.map) return null
+
+    // avoid adding/removing the marker to the map repeatedly
+    if (props.map === this.map) {
+      props = blacklist(props, 'map')
+    }
+
     // create new marker
     if (!this.marker) {
-      this.marker = new google.maps.Marker(this.props)
+      this.marker = new google.maps.Marker(props)
+      this.map = props.map
 
     // update existing marker
     } else {
-      this.marker.setOptions(this.props)
+      this.marker.setOptions(props)
     }
 
     return null
--- test/index.js
+++ test/index.js
@@ -3,20 +3,46 @@ var GoogleMap = require('../')
 
 var App = React.createClass({
   getInitialState: function () {
-    return {}
+    return {
+      t: 0,
+      center: {
+        lat: -37.9,
+        lng: 145.08
+      },
+      marker: {
+        lat: -37.9,
+        lng: 145.08
+      }
+    }
+  },
+
+  componentDidMount: function () {
+    var self = this
+
+    setInterval(function () {
+      var dx = 0.2 * Math.cos(self.state.t)
+      var dy = 0.2 * Math.sin(self.state.t)
+
+      self.setState({
+        t: self.state.t + 0.01,
+        marker: {
+          lng: self.state.center.lng + dx,
+          lat: self.state.center.lat + dy
+        }
+      })
+    }, 10)
   },
 
   render: function () {
-    var center = {
-      lat: -37.8602828,
-      lng: 145.079616
-    }
-
     return React.createElement(GoogleMap, {
-      center: center,
+      center: this.state.center,
       zoom: 8
     }, [
-      React.createElement(GoogleMap.Marker, { position: center, label: 'A pleasant place to live' })
+      React.createElement(GoogleMap.Marker, {
+        key: 'marker1',
+        position: this.state.marker,
+        label: 'A pleasant place to live'
+      })
     ])
   }
 })
