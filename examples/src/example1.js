8edad28ea2ca565cc806a2142606e60fc0d06068
Daniel Cousens <github@dcousens.com>
2015-07-31 16:34:09 +1000

    add onClick example for markers

--- examples/src/example1.js
+++ examples/src/example1.js
@@ -31,17 +31,22 @@ let App = React.createClass({
     }, 10)
   },
 
+  onClickMarker () {
+    this.setState({ markerInfoOpen: !this.state.markerInfoOpen })
+  },
+
   render () {
+    let { center, marker, markerInfoOpen } = this.state
     let marker1 = this.refs.marker1
 
     return (
-      <GoogleMap center={this.state.center} zoom={8}>
-        <GoogleMap.Marker ref='marker1' position={this.state.marker} label={'A pleasant place to live'} />
-        <GoogleMap.InfoWindow anchor={marker1 && marker1.getMarker()} open>
+      <GoogleMap center={center} zoom={8}>
+        <GoogleMap.Marker ref='marker1' position={marker} onClick={this.onClickMarker} label={'A pleasant place to live'} />
+        <GoogleMap.InfoWindow anchor={marker1 && marker1.getMarker()} open={markerInfoOpen} >
           <h4>Marker</h4>
         </GoogleMap.InfoWindow>
 
-        <GoogleMap.InfoWindow position={this.state.center} open>
+        <GoogleMap.InfoWindow position={center} open>
           <h2>Center</h2>
         </GoogleMap.InfoWindow>
       </GoogleMap>
